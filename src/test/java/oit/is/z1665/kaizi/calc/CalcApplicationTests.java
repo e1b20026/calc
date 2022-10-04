ca056edc88111e01593e726b3ad1a56d70579eaf
e1b20026 <e1b20026@oit.ac.jp>
2022-10-04 14:26:46 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z1665/kaizi/calc/CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1665.kaizi.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
